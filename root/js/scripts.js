fc57b5fd2a6d566b2517a15af701cabf8854e9ea
Yousif Alnajjar <yousif.j.alnajjar@gmail.com>
2022-11-06 09:29:21 -0500

    local 32

---
+++ root/js/scripts.js
@@ -0,0 +1,4 @@
+function display() {
+  document.getElementById("bottomTextDisplay").innerText = document.getElementById("bottomTextArea").value;
+  document.getElementById("topTextDisplay").innerText = document.getElementById("topTextArea").value;
+}
